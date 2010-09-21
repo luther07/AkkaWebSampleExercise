40e3572d122dc6d916f3bd97a82aa8ac83a0a419
Mark Johnson <luther07@gmail.com>
2010-09-21 14:04:48 -0500

    Got and committed current ActorSupervision file

--- src/main/scala/server/ActorSupervision.scala
+++ src/main/scala/server/ActorSupervision.scala
@@ -3,46 +3,21 @@ import se.scalablesolutions.akka.actor._
 import se.scalablesolutions.akka.actor.Actor._
 import se.scalablesolutions.akka.util.Logging
 
-
-sealed trait ActorManagementMessage
-case class GetActorFor(name: String)    extends ActorManagementMessage
-case class Register(actor: ActorRef)    extends ActorManagementMessage
-case class Unregister(actor: ActorRef)  extends ActorManagementMessage
-
-trait ActorSupervision extends Actor with Logging {
-  
-  def handleManagementMessage: PartialFunction[Any,Unit] = {
-    case Register(actor)   => doRegister(actor)
-
-    case Unregister(actor) => doUnregister(actor)
-  }
-
-  protected def doRegister(actor: ActorRef) = {
-    log.info("Registering actor: "+actor)
-    self link actor
-    Pair("message", "Actor registered")
-  }
-
-  def doUnregister(actor: ActorRef) = {
-    log.info("Registering actor: "+actor)
-    self unlink actor
-    Pair("message", "Actor unregistered")
-  }
-}
-
-object ActorSupervision {
-  
-  def getOrMakeActorFor(actorId: String, supervisorForMadeActor: Option[ActorRef])(makeActor: (String) => ActorRef) = 
+/**
+ * Methods to assist in the dynamic creation and supervision of subordinate actors.
+ */
+trait ActorSupervision extends Actor { this:Logging =>
+    
+  def getOrMakeActorFor(actorId: String)(makeActor: (String) => Actor): ActorRef = 
     ActorRegistry.actorsFor(actorId).toList match {
       case Nil => 
-        log.info("Creating new actor: "+actorId)
-        val actorRef = makeActor(actorId)
+        log.info("Created new actor with id "+actorId+".")
+        val actorRef = actorOf(makeActor(actorId))
         actorRef.id = actorId
         actorRef.start
-        supervisorForMadeActor match {
-          case None => log.warning("No supervisor specified for new actor " + actorId)
-          case Some(s) => s ! Register(actorRef)
-        }
+        log.info("Registering actor.")
+        self link actorRef
+        log.info("Returning new actor.")
         actorRef
       case head :: tail => 
         if (tail != Nil) log.warning("More than one actor exists with id " + actorId)
